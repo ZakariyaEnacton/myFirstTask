bcc9e2ff9cc3a7c2e2b1fdcb8a1c212fc58fe8e6
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-09 16:56:42 +0530

    feat : sighn up screen

--- src/Screens/SignUp/index.js
+++ src/Screens/SignUp/index.js
@@ -1,42 +1,90 @@
-import {View, Text, TextInput, TouchableOpacity, StatusBar} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  Image,
+} from 'react-native';
 import React from 'react';
 import en from '../../translations/en.json';
 import signupStyle from './style';
 import style from './style';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '../../Assets/Theme/colors';
+import {AppImages} from '../../Assets/Images';
 
 const SignUp = () => {
   return (
-    <View style={signupStyle.container}>
-      <View style={signupStyle.wrapper}>
-        <View style={signupStyle.headWrapper}>
-          <Text>{en.create_acc}</Text>
-          <Text>{en.start_now}</Text>
+    <LinearGradient style={signupStyle.container} colors={Colors.splashColor}>
+      <StatusBar barStyle={'light-content'} backgroundColor={Colors.purple} />
+      <View>
+        <View style={signupStyle.wrapper}>
+          <View style={signupStyle.headWrapper}>
+            <Text style={style.title}>{en.create_acc}</Text>
+            <Text style={style.titleStart}>{en.start_now}</Text>
+          </View>
+          <View style={signupStyle.inputWrapper}>
+            <TextInput
+              placeholderTextColor={Colors.white}
+              style={signupStyle.input}
+              placeholder={en.email_place_holder}
+              cursorColor={Colors.white}
+            />
+          </View>
+          <View>
+            <TextInput
+              placeholderTextColor={Colors.white}
+              style={signupStyle.input}
+              placeholder={en.pass_place_holder}
+              cursorColor={Colors.white}
+            />
+          </View>
+          <View>
+            <TextInput
+              placeholderTextColor={Colors.white}
+              style={signupStyle.input}
+              placeholder={en.confirm_pass_place_holder}
+              cursorColor={Colors.white}
+            />
+          </View>
+          <View style={style.buttonWrapper}>
+            <TouchableOpacity style={style.button}>
+              <Text style={style.btnText}>{en.sign_up}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={signupStyle.inputWrapper}>
-          <TextInput
-            style={signupStyle.input}
-            placeholder={en.email_place_holder}
-          />
+        <View style={style.navViewWrapper}>
+          <View style={style.horizontalView} />
+          <View>
+            <Text style={style.navTxt}>{en.navText}</Text>
+          </View>
+          <View style={style.horizontalView} />
         </View>
+        <View style={style.mediaBtnWrapper}>
+          <View>
+            <TouchableOpacity style={[style.btnMedia, {marginLeft: 25}]}>
+              <Image source={AppImages.googleImg} />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity style={[style.btnMedia, {marginRight: 25}]}>
+              <Image source={AppImages.facebookImg} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View style={style.navLinkTxtWrapper}>
         <View>
-          <TextInput
-            style={signupStyle.input}
-            placeholder={en.pass_place_holder}
-          />
+          <Text style={style.navLinkTxt}>{en.navLinkTxt}</Text>
         </View>
-        <View>
-          <TextInput
-            style={signupStyle.input}
-            placeholder={en.confirm_pass_place_holder}
-          />
-        </View>
-        <View style={style.buttonWrapper}>
+        <View style={{marginLeft: 2}}>
           <TouchableOpacity>
-            <Text>{en.sign_up}</Text>
+            <Text style={style.navLinkBtn}>Login Now</Text>
           </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </LinearGradient>
   );
 };
 
--- src/Screens/SignUp/style.js
+++ src/Screens/SignUp/style.js
@@ -1,35 +1,112 @@
 import {StyleSheet} from 'react-native';
-import colors from '../../Assets/Theme/colors';
+import {Colors} from '../../Assets/Theme/colors';
+import {fontStyle} from '../../Assets/Theme/fontStyle';
 
 export default signupStyle = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#8eadff',
+    backgroundColor: Colors.splashColor,
   },
   wrapper: {
     alignItems: 'center',
   },
   headWrapper: {
     alignItems: 'center',
+    marginBottom: 10,
+  },
+  title: {
+    ...fontStyle.headingBold,
+    color: Colors.white,
+  },
+  titleStart: {
+    ...fontStyle.headingRegular,
+    fontWeight: 300,
+    color: Colors.white,
   },
   inputWrapper: {
     marginTop: 5,
   },
   input: {
     borderWidth: 1,
-    borderColor: '#fff',
+    borderColor: Colors.white,
     margin: 5,
     width: 350,
     borderRadius: 10,
+    padding: 10,
+    backgroundColor: Colors.lightViolet,
+    shadowColor: Colors.black,
+    elevation: 2,
+    color: Colors.white,
   },
   buttonWrapper: {
     marginTop: 20,
-    backgroundColor: '#fff',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: Colors.white,
     padding: 15,
     width: 350,
     borderRadius: 10,
-    shadowColor: '#000',
-    elevation: 10,
+    shadowColor: Colors.black,
+    elevation: 2,
+    color: Colors.black,
+  },
+  btnText: {
+    ...fontStyle.h3Bold,
+    color: Colors.black,
+  },
+  navViewWrapper: {
+    marginTop: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  navView: {
+    height: 0.5,
+    backgroundColor: Colors.white,
+    flex: 1,
+  },
+  horizontalView: {
+    flex: 1,
+    height: 0.5,
+    backgroundColor: Colors.white,
+  },
+  navTxt: {
+    textAlign: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    color: Colors.white,
+  },
+  mediaBtnWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 25,
+  },
+  btnMedia: {
+    backgroundColor: Colors.white,
+    padding: 12,
+    width: 165,
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: Colors.black,
+    elevation: 2,
+  },
+  navLinkTxtWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    left: 90,
+  },
+  navLinkTxt: {
+    color: Colors.white,
+    fontWeight: 300,
+  },
+  navLinkBtn: {
+    textDecorationLine: 'underline',
+    color: Colors.white,
+    fontWeight: 500,
   },
 });
