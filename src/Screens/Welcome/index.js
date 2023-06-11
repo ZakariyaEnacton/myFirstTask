69af1e99e8d7dfe65251e20285f69384a6d37a3e
ZakariyaEnacton <zakriyabombaywala@gmail.com>
2023-06-11 10:56:02 +0530

    feat : welcome screen

--- src/Screens/Welcome/index.js
+++ src/Screens/Welcome/index.js
@@ -1,12 +1,4 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  Image,
-  ScrollViewBase,
-  ScrollViewComponent,
-  TouchableOpacity,
-} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StatusBar} from 'react-native';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import {Colors} from '../../Assets/Theme/colors';
@@ -17,12 +9,13 @@ import en from '../../translations/en.json';
 const Welcome = () => {
   return (
     <LinearGradient style={style.container} colors={Colors.splashColor}>
+      <StatusBar hidden={true} />
       <View style={style.viewWrapper}>
         <View>
           <Image source={AppImages.appLogo} style={style.image} />
         </View>
         <View style={style.appTxtWrapper}>
-          <Text style={style.appTxt}>PixelPro Digital</Text>
+          <Text style={style.appTxt}>{en.app_name}</Text>
         </View>
         <View style={[style.buttonWrapper, {top: 10}]}>
           <TouchableOpacity style={[style.button, style.btnLogin]}>
