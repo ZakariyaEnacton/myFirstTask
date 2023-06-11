a2f529887cbfef5797d8b9a8ec056933167579bd
ZakariyaEnacton <zakriyabombaywala@gmail.com>
2023-06-11 10:38:50 +0530

    feat : welcome screen

--- src/Screens/Welcome/index.js
+++ src/Screens/Welcome/index.js
@@ -1,11 +1,51 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  ScrollViewBase,
+  ScrollViewComponent,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '../../Assets/Theme/colors';
+import {AppImages} from '../../Assets/Images';
+import style from './style';
+import en from '../../translations/en.json';
 
 const Welcome = () => {
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <LinearGradient style={style.container} colors={Colors.splashColor}>
+      <View style={style.viewWrapper}>
+        <View>
+          <Image source={AppImages.appLogo} style={style.image} />
+        </View>
+        <View style={style.appTxtWrapper}>
+          <Text style={style.appTxt}>PixelPro Digital</Text>
+        </View>
+        <View style={[style.buttonWrapper, {top: 10}]}>
+          <TouchableOpacity style={[style.button, style.btnLogin]}>
+            <Text style={[style.btnText, {color: Colors.black}]}>
+              {en.login}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={[style.buttonWrapper, {top: 30}]}>
+          <TouchableOpacity style={[style.button, style.btnSignUp]}>
+            <Text
+              style={[style.btnText, {color: Colors.white, fontWeight: 400}]}>
+              {en.sign_up}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={style.txtWrapper}>
+          <TouchableOpacity>
+            <Text style={style.text}>{en.guest}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </LinearGradient>
   );
 };
 
--- src/Screens/Welcome/style.js
+++ src/Screens/Welcome/style.js
@@ -1,1 +1,58 @@
-export default style = StyleSheet.create({});
+import {StyleSheet} from 'react-native';
+import {Colors} from '../../Assets/Theme/colors';
+import {fontStyle} from '../../Assets/Theme/fontStyle';
+
+export default style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  viewWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: 100,
+    width: 100,
+    position: 'relative',
+    bottom: 50,
+  },
+  appTxtWrapper: {
+    position: 'relative',
+    bottom: 50,
+  },
+  appTxt: {
+    ...fontStyle.headingBold,
+    color: Colors.white,
+    fontSize: 24,
+  },
+  buttonWrapper: {
+    position: 'relative',
+  },
+  button: {
+    width: 350,
+    borderRadius: 10,
+    borderColor: Colors.white,
+    alignItems: 'center',
+    padding: 12,
+    // borderWidth: 1,
+  },
+  btnLogin: {
+    backgroundColor: Colors.white,
+  },
+  btnSignUp: {
+    borderWidth: 1,
+  },
+  btnText: {
+    ...fontStyle.h2Ragular,
+    fontWeight: 500,
+  },
+  txtWrapper: {
+    position: 'absolute',
+    bottom: 40,
+  },
+  text: {
+    color: Colors.white,
+    textDecorationLine: 'underline',
+  },
+});
