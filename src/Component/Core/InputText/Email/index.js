e808b7ce1032484e3d820b35a24f5d0e5b5acfdf
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-12 10:14:43 +0530

    feat : Email TextInput

---
+++ src/Component/Core/InputText/Email/index.js
@@ -0,0 +1,20 @@
+import {View, Text, TextInput} from 'react-native';
+import React from 'react';
+import en from '../../../../translations/en.json';
+import {Colors} from '../../../../Assets/Theme/colors';
+import style from './style';
+
+const Email = (placeHolder, placeholderTextColor) => {
+  return (
+    <View>
+      <TextInput
+        style={[style.input, {marginBottom: 10}]}
+        placeholder={en.email_place_holder}
+        placeholderTextColor={Colors.white}
+        cursorColor={Colors.white}
+      />
+    </View>
+  );
+};
+
+export default Email;
