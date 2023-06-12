8084ce3ef96ca446123db3b481c13cd9f3992ca2
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-06-12 10:14:58 +0530

    feat : Login Component

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,28 +1,55 @@
-import {View, Text, ScrollView, StatusBar, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  StatusBar,
+  SafeAreaView,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import {Colors} from '../../Assets/Theme/colors';
+import en from '../../translations/en.json';
+import style from '../Login/style';
+import {fontStyle} from '../../Assets/Theme/fontStyle';
+import InputText from '../../Component/Core/InputText/Email';
+import Email from '../../Component/Core/InputText/Email';
+import Password from '../../Component/Core/InputText/Password';
 
 const index = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <LinearGradient
-        style={{flex: 1, justifyContent: 'center'}}
-        colors={Colors.splashColor}>
-        <ScrollView style={{backgroundColor: '#fff'}}>
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'center',
-              backgroundColor: '#ffedde',
-              justifyContent: 'center',
-            }}>
-            <Text>Hello</Text>
-          </View>
-        </ScrollView>
-      </LinearGradient>
-      <StatusBar backgroundColor={Colors.purple} />
-    </SafeAreaView>
+    <LinearGradient colors={Colors.splashColor} style={style.container}>
+      <View style={style.headWrapper}>
+        <Text style={[style.headTxt, {...fontStyle.headingBold28}]}>
+          {en.welcome},
+        </Text>
+        <Text
+          style={[
+            style.headTxt,
+            {...fontStyle.headingRegular28, fontWeight: 300},
+          ]}>
+          {en.greeting}
+        </Text>
+      </View>
+      <View>
+        <Email />
+      </View>
+      <View>
+        <Password />
+      </View>
+      <View style={style.frgPassWrapper}>
+        <TouchableOpacity>
+          <Text style={style.frgPassTxt}>{en.forgot_pass}</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>{en.login}</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 };
 
---
+++ src/Screens/Login/style.js
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from '../../Assets/Theme/colors';
+import {fontStyle} from '../../Assets/Theme/fontStyle';
+
+export default style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  headWrapper: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  headTxt: {
+    color: Colors.white,
+  },
+  input: {
+    borderWidth: 1,
+    width: 350,
+    alignSelf: 'center',
+    backgroundColor: Colors.lightViolet,
+    borderColor: Colors.white,
+    borderRadius: 10,
+    shadowColor: Colors.black,
+    elevation: 2,
+    padding: 10,
+  },
+  frgPassWrapper: {
+    alignItems: 'flex-end',
+    width: 370,
+    marginTop: 10,
+  },
+  frgPassTxt: {
+    ...fontStyle.h3Ragular,
+    textDecorationLine: 'underline',
+    color: Colors.white,
+  },
+});
